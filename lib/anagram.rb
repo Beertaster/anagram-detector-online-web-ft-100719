3c46428a9a63a4aa8be6ee11f0d6c9f890655aee
William Stevens <wstevens.hornet@gmail.com>
2019-10-21 04:38:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,12 +2,12 @@
 class Anagram
   attr_accessor :word_to_analyze
   
-  def initialize(word_to_analyze)
-    @word_to_analyze = word_to_analyze 
+  def initialize(words)
+    @word_to_analyze = words 
   end 
   
   def match(string) 
-    puts "Must find #{self.word_to_analyze} inside the following string: #{string}"
+    puts "Must find #{self.words} inside the following string: #{string}"
     string.find_all do |word| 
       if word.split("").sort == self.word_to_analyze.split("").sort 
         word 
