302f76861d4c070f05af9ead3416ba604f34d1c5
William Stevens <wstevens.hornet@gmail.com>
2019-10-21 03:31:57 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
   end 
   
   def match(string) 
-    puts "Must find #{self.word_to_analyze} inside the following string: #{string}"
+    puts "Must find #{self.word} inside the following string: #{string}"
     string.find_all do |word| 
       if word.split("").sort == self.word_to_analyze.split("").sort 
         word 
