0bdb604a8debd8250928684cab9c2c40ef953933
William Stevens <wstevens.hornet@gmail.com>
2019-10-21 03:30:49 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,9 +6,9 @@ class Anagram
     @word_to_analyze = word_to_analyze 
   end 
   
-  def match(find_anagram_in_this_string) 
-    puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
-    find_anagram_in_this_string.find_all do |word| 
+  def match(string) 
+    puts "Must find #{self.word_to_analyze} inside the following string: #{string}"
+    string.find_all do |word| 
       if word.split("").sort == self.word_to_analyze.split("").sort 
         word 
       end
