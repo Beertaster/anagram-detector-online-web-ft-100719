ec6a5a7a4b0464f658ff1ef23c49e76a6f0f8b53
William Stevens <wstevens.hornet@gmail.com>
2019-10-21 06:33:13 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,15 @@
 # Your code goes here!
 class Anagram
-  attr_accessor :words
+  attr_accessor :word_to_analyze
   
-  def initialize(words)
-    @words = words 
+  def initialize(word_to_analyze)
+    @word_to_analyze = word_to_analyze 
   end 
   
   def match(string) 
-    puts "Must find #{self.words} inside the following string: #{string}"
-    string.find_all do |word| 
-      if words.split("").sort == self.words.split("").sort 
+    puts "Must find #{self.word_to_analyze} inside the following string: #{string}"
+      string.find_all do |word| 
+      if word.split("").sort == self.word_to_analyze.split("").sort 
         word 
       end
     end 
